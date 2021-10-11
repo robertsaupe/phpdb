9f307ad5c7fdd8fac0d5646964b6413d6c7d6b6d
Robert Saupe <robert@mysaupe.de>
2021-10-11 10:40:11 +0200

    2.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 2.2.0 - 2021-10-11
+- namespace correction
+
 ## 2.1.0 - 2021-05-04
 - coding standards
 
--- src/mysqli.php
+++ src/mysqli.php
@@ -10,55 +10,59 @@
  * @license MIT License
  */
 
-namespace RobertSaupe\DB;
+namespace robertsaupe\db;
 
 use mysqli as phpmysqli;
 
 /**
  * implements connection to mysql with mysqli
  */
-abstract class MySQLi {
+abstract class mysqli {
 
-	private phpmysqli $conn;
+	private phpmysqli $connection;
 	private string $table;
 	private false|string $error = false;
     
 	protected function __construct(string $server, string $user, string $password, string $database, string $prefix, string $table) {
 		$this->table = $prefix . $table;
-		$this->conn = new phpmysqli($server, $user, $password, $database);
-		if ($this->conn->connect_errno) $this->error = "Failed to connect to MySQL: " . $this->conn->connect_error;
+		$this->connection = new phpmysqli($server, $user, $password, $database);
+		if ($this->connection->connect_errno) $this->error = "Failed to connect to MySQL: " . $this->connection->connect_error;
 	}
 
-	public function getError() {
+	public function get_connection() {
+		return $this->connection;
+	}
+
+	public function get_error() {
 		return $this->error;
 	}
 
-	public function getTableName() {
+	public function get_table_name() {
 		return $this->table;
 	}
 
 	protected function real_escape_string($string) {
-		return $this->conn->real_escape_string($string);
+		return $this->connection->real_escape_string($string);
 	}
 	
 	protected function query($string) {
-		return $this->conn->query("$string");
+		return $this->connection->query("$string");
     }
     
 	protected function insert($insert) {
-		return $this->conn->query("insert into $this->table $insert");
+		return $this->connection->query("insert into $this->table $insert");
     }
     
 	protected function result($where) {
-		return $this->conn->query("select * from $this->table where $where");
+		return $this->connection->query("select * from $this->table where $where");
     }
     
 	protected function update($update) {
-		return $this->conn->query("update $this->table set $update");
+		return $this->connection->query("update $this->table set $update");
     }
     
 	protected function delete($where) {
-		return $this->conn->query("delete from $this->table where $where");
+		return $this->connection->query("delete from $this->table where $where");
 	}
 
 }
